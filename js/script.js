3f275bded1686a95934b349123db49334bd1241a
AmauryFaveriel <mrfvrl@gmail.com>
2017-12-08 22:15:05 +0100

    modif script et style

--- js/script.js
+++ js/script.js
@@ -8,4 +8,22 @@
      column.innerHTML+='<li class="box" id='+((i+1)*100+(n+1))+'></li>'
    }
  }
- var modifiedBox=document.getElementById(101);
+ var a=100;
+ var b=1;
+ var modifiedBox=document.getElementById(a+b);
+ modifiedBox.className="boxModified";
+ window.addEventListener('keydown', function(event){
+   var modifiedBox=document.getElementById(a+b);
+   modifiedBox.className="box";
+   if(event.which===40 && b<20){
+     b+=1;
+   }
+   else if(event.which===39 && a<1000){
+     a+=100;
+   }
+   else if(event.which===37 && a>100){
+     a-=100;
+   }
+   var modifiedBox=document.getElementById(a+b);
+   modifiedBox.className="boxModified";
+ })
